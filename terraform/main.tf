21a501d443c9aaefa85e1ea8b3359796a412d4aa
Akshith17323 <akhi171356@gmail.com>
2026-05-05 12:02:14 +0530

    remove manual S3 bucket import step from CI/CD pipeline

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,6 +6,12 @@ terraform {
     }
   }
   required_version = ">= 1.3.0"
+
+  backend "s3" {
+    bucket = "shopsmart-bucket-akshith17323"
+    key    = "terraform/state.tfstate"
+    region = "us-east-1"
+  }
 }
 
 provider "aws" {
